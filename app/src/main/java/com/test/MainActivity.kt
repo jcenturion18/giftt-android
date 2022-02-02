5f116a68c5340256fb904ee60215f4e0f05dbb8d
Julian Centurion <julian.centurion@mercadolibre.com>
2022-02-02 16:59:27 -0300

    Implement activity

--- app/src/main/java/com/test/MainActivity.kt
+++ app/src/main/java/com/test/MainActivity.kt
@@ -1,16 +1,66 @@
 package com.test
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.test.database.DataBaseInterface
+import com.test.database.DatabaseImp
 import com.test.databinding.ActivityMainBinding
+import com.test.dto.User
 
-class MainActivity : AppCompatActivity() {
+class MainActivity(
+    private val db: DataBaseInterface = DatabaseImp()
+) : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    var lastUser: User? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.language.setOnClickListener {
+        setTextListener()
+        loadLastUserName()
+    }
+
+    private fun setTextListener() {
+        binding.language.setOnClickListener { }
+
+        binding.addUser.setOnClickListener {
+            showDialog()
         }
+    }
+
+    private fun loadLastUserName() {
+        db.loadUserName {
+            lastUser = it
+            val text = "${getString(R.string.last_user)} ${it.name}"
+            binding.lastUser.text = text
+        }
+    }
+
+    private fun showDialog() {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+
+        builder.setTitle(getString(R.string.enter_name))
+
+        val input = EditText(this)
+
+        input.hint = getString(R.string.name)
+        input.inputType = InputType.TYPE_CLASS_TEXT
+        builder.setView(input)
+
+        builder.setPositiveButton(getString(R.string.submit)) { _, _ ->
+            db.store(input.text.toString())
+        }
+        builder.setNegativeButton(getString(R.string.cancel)) { dialog, _ ->
+            dialog.cancel()
+        }
+
+        builder.show()
     }
 }
