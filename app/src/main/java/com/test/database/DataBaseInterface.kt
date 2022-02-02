d71bbb96e07619b69fa4a295639be2d7a51d931d
Julian Centurion <julian.centurion@mercadolibre.com>
2022-02-02 16:59:47 -0300

    Create basic functions

--- app/src/main/java/com/test/database/DataBaseInterface.kt
+++ app/src/main/java/com/test/database/DataBaseInterface.kt
@@ -1,8 +1,12 @@
 /**
-* Created by Julian Centurion on 02/02/2022.
-*/
+ * Created by Julian Centurion on 02/02/2022.
+ */
 
 package com.test.database
 
+import com.test.dto.User
+
 interface DataBaseInterface {
+    fun store(userName: String)
+    fun loadUserName(callback: (User) -> Unit)
 }
